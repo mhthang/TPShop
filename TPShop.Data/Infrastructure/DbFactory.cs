3570d6cda305391f17cbe4fc408be33f5d0e2c2d
mhthang <maihuuthang.mht@gmail.com>
2016-07-20 21:11:39 +0700

    add data access

---
+++ TPShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPShop.Data.Infrastructure
+{
+    public class DbFactory:Disposable,IDbFactory
+    {
+        TPShopDbContext dbcontext;
+        public TPShopDbContext Init()
+        {
+            return dbcontext ?? (dbcontext = new TPShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbcontext!=null)
+            {
+                dbcontext.Dispose();
+            }
+        }
+    }
+}
---
+++ TPShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPShop.Data.Infrastructure
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private TPShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public TPShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
